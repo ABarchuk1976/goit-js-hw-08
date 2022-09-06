eefb3a43f74825655cde20686a4ca01f4ea15987
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-07 00:32:23 +0300

    Load simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import { default as SimpleLightbox } from '../../node_modules/simplelightbox/src/simple-lightbox.js';
 // Change code below this line
+// const SimpleLightbox = require('simple-lightbox');
+const lightbox = new SimpleLightbox('.gallery a');
 
-console.log(galleryItems);
+console.log(lightbox.defaultOptions);
