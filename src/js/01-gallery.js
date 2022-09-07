d05151dda9ba01b5a05a70711c3e0e50f14d0399
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-07 10:33:36 +0300

    Fixed 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,35 @@ import { galleryItems } from './gallery-items.js';
 import { default as SimpleLightbox } from '../../node_modules/simplelightbox/src/simple-lightbox.js';
 // Change code below this line
 // const SimpleLightbox = require('simple-lightbox');
-const lightbox = new SimpleLightbox('.gallery a');
+const galleryRef = document.querySelector('.gallery');
+let markup = '';
+
+for (const img of galleryItems) {
+  markup += `<a class="gallery__item" href=${img.original} title="${img.description}">
+			<img 
+				class="gallery__image"
+				src=${img.preview} 
+				alt="${img.description}"
+			/>
+			</a>`;
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+const paramsLightbox = {
+  overlayOpacity: 1,
+  captionSelector: 'self',
+  captionDelay: 250,
+};
+const lightbox = new SimpleLightbox('.gallery a', paramsLightbox);
 
 console.log(lightbox.defaultOptions);
+
+lightbox.close();
+
+galleryRef.addEventListener('click', event => {
+  if (event.target.tagName !== 'IMG') return;
+
+  lightbox.show;
+
+  event.preventDefault();
+});
