1c3ec175e6191a68be575f858c92d5eba3cd6a75
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-07 23:44:17 +0300

    Commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,35 @@
+import { throttle } from 'lodash';
 import { default as Player } from '../../node_modules/@vimeo/player/src/player.js';
+import { throttle } from '../../node_modules/lodash/throttle.js';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-const key = 'seconds';
+const key = 'videoplayer-current-time';
+const _ = require('../../node_modules/lodash/lodash.js');
+const throttled = _.throttle(function () {
+  player.getCurrentTime(key);
+}, 1000);
 
-player.on('pause', () => {
-  player.getCurrentTime().then(result => {
+player.on('timeupdate', () => {
+  throttled.then(result => {
     try {
+      console.log('Pause at: ', result);
       localStorage.setItem(key, String(result));
     } catch (error) {
       console.error('Помилка збереження даних: ', error.message);
     }
-  });
 
-  const aValue = localStorage.getItem(key);
-  console.log('In localStorage: ', Number(aValue));
+    const aValue = localStorage.getItem(key);
+    console.log('In localStorage: ', Number(aValue));
+  });
 });
 
 player.on('play', () => {
   let aValue = 0;
   try {
     const res = localStorage.getItem(key);
-    console.log(res);
+    console.log(typeof res);
+    console.log('Start from: ', res);
     if (res !== null) aValue = Number(res);
   } catch (error) {
     console.error('Помилка завантаження даних: ', error.message);
