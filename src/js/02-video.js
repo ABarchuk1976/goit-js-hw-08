78f03ee1887cb1b42195e5eac24951b94bc7d91d
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-09 00:12:53 +0300

    Fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,41 +1,40 @@
 import { throttle } from 'lodash';
-import { default as Player } from '../../node_modules/@vimeo/player/src/player.js';
+const _ = require('../../node_modules/lodash/lodash.js');
+import Player from '../../node_modules/@vimeo/player/src/player.js';
 import { throttle } from '../../node_modules/lodash/throttle.js';
 
 const iframe = document.querySelector('#vimeo-player');
+
 const player = new Player(iframe);
 const key = 'videoplayer-current-time';
-const _ = require('../../node_modules/lodash/lodash.js');
-const throttled = _.throttle(function () {
-  player.getCurrentTime(key);
+let currentTime = 0;
+
+const throttled = _.throttle(() => {
+  try {
+    player.getEnded().then(ended => {
+      if (!ended) {
+        player.getCurrentTime().then(seconds => {
+          currentTime = seconds;
+        });
+      } else {
+        currentTime = 0;
+      }
+
+      localStorage.setItem(key, String(currentTime));
+    });
+  } catch (error) {
+    console.error('Помилка збереження даних: ', error.message);
+  }
 }, 1000);
 
-player.on('timeupdate', () => {
-  throttled.then(result => {
-    try {
-      console.log('Pause at: ', result);
-      localStorage.setItem(key, String(result));
-    } catch (error) {
-      console.error('Помилка збереження даних: ', error.message);
-    }
-
-    const aValue = localStorage.getItem(key);
-    console.log('In localStorage: ', Number(aValue));
-  });
-});
+player.on('timeupdate', throttled);
 
 player.on('play', () => {
-  let aValue = 0;
   try {
-    const res = localStorage.getItem(key);
-    console.log(typeof res);
-    console.log('Start from: ', res);
-    if (res !== null) aValue = Number(res);
+    currentTime = localStorage.getItem(key);
+    console.log('Play from: ', currentTime);
+    player.setCurrentTime(Number(currentTime));
   } catch (error) {
     console.error('Помилка завантаження даних: ', error.message);
   }
-
-  player.setCurrentTime(aValue).then(result => {
-    result = aValue;
-  });
 });
